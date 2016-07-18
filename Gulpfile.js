a08171e2fba227d2f06b3944ea735c429cffa364
Dom Habersack <dominik.habersack@netlight.com>
2016-07-18 21:30:31 +0200

    Remove some test-output.

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,23 +4,19 @@ var gulp = require('gulp');
 
 gulp.task('connect', function () {
   connect.server({}, function () {
-    browserSync({
-    });
+    browserSync();
   });
 });
 
 gulp.task('default', ['connect', 'watch']);
 
 gulp.task('php', function () {
-  console.log('php-ing');
-
   gulp.src('site/**/*.php')
       .pipe(browserSync.reload());
 });
 
 gulp.task('watch', function () {
   gulp.watch('site/**/*.php').on('change', function () {
-    console.log('change to the PHP');
     browserSync.reload();
   });
 });
