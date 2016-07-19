482a7058bf0154dd62438be408e8b550aaf22e81
Dom Habersack <dominik.habersack@netlight.com>
2016-07-19 21:22:12 +0200

    Remove default Kirby-template, replace it with a few half-baked ideas.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title><?php echo $site->title()->html(); ?> | <?php echo $page->title()->html(); ?></title>
+    <meta name="description" content="<?php echo $site->description()->html(); ?>">
+    <meta name="keywords" content="<?php echo $site->keywords()->html(); ?>">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <meta name="description" content="<?php echo $site->description()->html() ?>">
-  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i" rel="stylesheet">
+    <?php echo css('assets/normalize.css') ?>
+    <?php echo css('assets/style.css') ?>
+  </head>
 
-  <?php echo css('assets/css/main.css') ?>
+  <body>
+    <header>
+      <a class="logo" href="<?php echo url(); ?>">
+        <?php echo $site->title(); ?>
+      </a>
 
-</head>
-<body>
-
-  <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
-    <?php snippet('menu') ?>
-  </header>
+      <?php snippet('menu'); ?>
+    </header>
