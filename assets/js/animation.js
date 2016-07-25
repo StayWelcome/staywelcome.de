c9d1b6b11fada872dce7b1a9767c26bd837b2811
Florian Schnell <florian.schnell@netlight.com>
2016-07-25 21:47:17 +0200

    added simple butterfly animation.

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,5 +1,59 @@
 import Snap from 'snapsvg';
 
-export default function () {
-    // here goes the SVG animation code...
+const WINGS_PAUSE_MIN_MILLIS = 2000;
+const WINGS_PAUSE_MAX_MILLIS = 10000;
+
+const WINGS_NUM_SUCCESSIVE_FLAPS_MIN = 1;
+const WINGS_NUM_SUCCESSIVE_FLAPS_MAX = 5;
+
+const FLAP_DURATION_MIN_MILLIS = 100;
+const FLAP_DURATION_MAX_MILLIS = 200;
+
+const identityMatrix = new Snap.Matrix();
+
+const leftWingTransform = new Snap.Matrix();
+leftWingTransform.scale(0.3, 0.8, 33.0184, 0);
+
+const rightWingTransform = new Snap.Matrix();
+rightWingTransform.scale(0.3, 0.8, 33.0184, 0);
+
+const bodyTransform = new Snap.Matrix();
+bodyTransform.scale(1.1);
+bodyTransform.translate(-4, -1, 33.0184, 0);
+
+const getRandomNumber = (from, to) => Math.random() * (to - from) + from;
+
+function flapWings(leftWing, rightWing, body, flaps) {
+    if (flaps <= 0) {
+        const timeout = getRandomNumber(WINGS_PAUSE_MIN_MILLIS, WINGS_PAUSE_MAX_MILLIS);
+        const flaps = getRandomNumber(WINGS_NUM_SUCCESSIVE_FLAPS_MIN, WINGS_NUM_SUCCESSIVE_FLAPS_MAX);
+
+        window.setTimeout(
+            flapWings.bind(null, leftWing, rightWing, body, flaps),
+            timeout
+        );
+    } else {
+        const time = getRandomNumber(FLAP_DURATION_MIN_MILLIS, FLAP_DURATION_MAX_MILLIS);
+
+        leftWing.animate({ transform: leftWingTransform }, time, mina.easeout, openWings.bind(null, leftWing, rightWing, body, flaps, time));
+        rightWing.animate( { transform: rightWingTransform }, time, mina.easeout );
+        body.animate( { transform: bodyTransform }, time, mina.easeout );
+    }
+}
+
+function openWings(leftWing, rightWing, body, flaps, time) {
+    leftWing.animate( { transform: identityMatrix }, time / 2, mina.easein, flapWings.bind(null, leftWing, rightWing, body, --flaps));
+    rightWing.animate( { transform: identityMatrix }, time / 2, mina.easein );
+    body.animate( { transform: identityMatrix }, time / 2, mina.easein );
+}
+
+export default function (svgElement) {
+    if (typeof svgElement !== 'undefined') {
+        const logo = new Snap(svgElement);
+        const wingLeft = logo.select('#wing-left');
+        const wingRight = logo.select('#wing-right');
+        const body = logo.select('#body');
+
+        flapWings(wingLeft, wingRight, body, 0);
+    }
 };
--- site/snippets/logo.php
+++ site/snippets/logo.php
@@ -1,5 +1,5 @@
-<a class="logo" href="<?php echo url(); ?>">
-  <object type="image/svg+xml" data="assets/svg/logo.svg" class="logo">
+<a href="<?php echo url(); ?>">
+  <object id="logoObject" type="image/svg+xml" data="assets/svg/logo.svg" class="logo">
       <?php echo $site->title(); ?>
   </object>
 </a>
