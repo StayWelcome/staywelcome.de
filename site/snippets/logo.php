1091e7d89d8cedeac0ac32d525a5b1c7cf5e43cc
Florian Schnell <florian.schnell@netlight.com>
2016-07-25 20:51:05 +0200

    adjusted logo for animations.

--- site/snippets/logo.php
+++ site/snippets/logo.php
@@ -1,3 +1,5 @@
 <a class="logo" href="<?php echo url(); ?>">
-  <?php echo $site->title(); ?>
+  <object type="image/svg+xml" data="assets/svg/logo.svg" class="logo">
+      <?php echo $site->title(); ?>
+  </object>
 </a>
